95d45c61b62252dc75be98f460618f06dc858c87
freddy roland <freddyrolandhinkamma877@gmail.com>
2026-08-25 17:41:58 +0200

    fix: enable localhost in CORS origins

--- config/cors.php
+++ config/cors.php
@@ -7,8 +7,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        // ' http://localhost:4200',
         'https://bank-connect-front.vercel.app',
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
+        env('FRONTEND_URL', ''), // Récupère aussi l'URL depuis le .env si elle existe
     ],
 
     'allowed_origins_patterns' => [],
